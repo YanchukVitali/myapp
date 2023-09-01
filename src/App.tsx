adcf234dbe75564101604f381c76ac1166aad374
Vital <Vitalii8881878@gmail.com>
2023-09-01 07:14:11 -0400

    commit1 - nevBranch add div3

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div>New tires</div>
+      <div>New rotors</div>
+      <div>New pads</div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>

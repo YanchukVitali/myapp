15e18e36685337c9a1b06bf45649ee4bbc00e8c3
Vital <Vitalii8881878@gmail.com>
2023-11-15 20:44:57 -0500

    refactor App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {NewComponent} from "./NewComponent";
 import {Cars} from "./Cars";
 import {log} from "util";
 import {Button} from "./components/Button";
 
-function App()  {
+function App() {
     /*   const students = [
            {id: 1, name: "Vit", age: 33},
            {id: 2, name: "Dima", age: 32},
@@ -23,24 +23,70 @@ function App()  {
            {id: 2,manufacturer: 'Mercedes', model: 'e63s'},
            {id: 3,manufacturer: 'Audi', model: 'rs6'}
        ]*/
-    const Button1Vit = (subscriber:string, age:number, address: string) => {
-        console.log(subscriber, age, address)
-    }
-    const Button2Vit = (subscriber:string) => {
-        console.log(subscriber)
-    }
-    const Button3Vit =()=>{
-        console.log("I am stupid Button ")
-    }
+    /*    const Button1Vit = (subscriber:string, age:number, address: string) => {
+            console.log(subscriber, age, address)
+        }
+        const Button2Vit = (subscriber:string) => {
+            console.log(subscriber)
+        }
+        const Button3Vit =()=>{
+            console.log("I am stupid Button ")
+        }*/
+
+
+
+
+
+    /*
+        let [a, setA] = useState(100  )
+        let b =0
+        const onClickHandler = ()=>{
+            setA(++a);
+            console.log(a)
+        }
+        const onClickHandler2 = ()=>{
+            setA(0)
+        }
+
+        const TestButton = (name:string)=>{
+            console.log(name)
+        }
+    */
+    const [money, setMoney] = useState([
+        {banknote: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknote: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknote: 'RUBLS', value: 100, number: ' w1234567890'},
+        {banknote: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknote: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknote: 'RUBLS', value: 100, number: ' r1234567890'},
+        {banknote: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknote: 'RUBLS', value: 50, number: ' v1234567890'},
+    ])
 
     return (
         <div className="App">
-            {/* <NewComponent students={students}/>
-            <Cars topCars={topCars}/>*/}
-            {/*<NewButton/>*/}
+            <ul>
+                {money.map((objFromMoneyArr, index  )=>{
+                    return (
+                        <li key={index}>
+                            <span>{objFromMoneyArr.banknote}</span>
+                            <span>{objFromMoneyArr.value}</span>
+                            <span>{objFromMoneyArr.number}</span>
+                        </li>
+                    )
+                })}
+
+            </ul>
+            {/*    <NewComponent students={students}/>
+            <Cars topCars={topCars}/>
+            <NewButton/>
+            <h1>{a}</h1>
+            <button onClick={onClickHandler}>number</button>
+            <button onClick={onClickHandler2}>0</button>
             <Button name={"Favorite-1"} callBack={()=>Button1Vit("I am Vit", 21, "Live in Boston")}/>
             <Button name={"Favorite-2"} callBack={()=>Button2Vit("I am Anna")}/>
             <Button name={"Stupid Button"} callBack={Button3Vit}/>
+            <Button name={"Test"} callBack={()=>TestButton("Test is good")}/>*/}
         </div>
     )
 }
@@ -70,4 +116,5 @@ function App()  {
         </div>
     )
 }*/
+
 export default App;
